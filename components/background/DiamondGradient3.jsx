e8962985c0c5890eae868c5d306953da210da827
jerry <10166007@ntub.edu.tw>
2025-02-24 12:38:48 +0800

    style：modify DiamondGradient3 component

--- components/background/DiamondGradient3.jsx
+++ components/background/DiamondGradient3.jsx
@@ -2,16 +2,15 @@
 
 import React from 'react';
 
-const Test = ({ width = 200, height = 200 }) => {
+const DiamondGradient3 = ({ width = 200, height = 200 }) => {
   return (
-    <div className={'relative left-[150px] top-0'}>
+    <div className={'flex items-center justify-center opacity-80 blur-[25px]'}>
       <svg
         xmlns='http://www.w3.org/2000/svg'
-        width={width}
-        height={height}
+        width='560'
+        height='906'
         viewBox='0 0 560 906'
         fill='none'
-        className={'absolute -left-[308px] top-0'}
       >
         <g
           clip-path='url(#paint0_diamond_1418_1526_clip_path)'
@@ -24,7 +23,7 @@ const Test = ({ width = 200, height = 200 }) => {
               width='1157.29'
               height='1012.33'
               fill='url(#paint0_diamond_1418_1526)'
-              opacity='1'
+              opacity='0.8'
               shape-rendering='crispEdges'
             />
             <rect
@@ -34,7 +33,7 @@ const Test = ({ width = 200, height = 200 }) => {
               height='1012.33'
               transform='scale(1 -1)'
               fill='url(#paint0_diamond_1418_1526)'
-              opacity='1'
+              opacity='0.8'
               shape-rendering='crispEdges'
             />
             <rect
@@ -44,7 +43,7 @@ const Test = ({ width = 200, height = 200 }) => {
               height='1012.33'
               transform='scale(-1 1)'
               fill='url(#paint0_diamond_1418_1526)'
-              opacity='1'
+              opacity='0.8'
               shape-rendering='crispEdges'
             />
             <rect
@@ -54,14 +53,14 @@ const Test = ({ width = 200, height = 200 }) => {
               height='1012.33'
               transform='scale(-1)'
               fill='url(#paint0_diamond_1418_1526)'
-              opacity='1'
+              opacity='0.8'
               shape-rendering='crispEdges'
             />
           </g>
         </g>
         <path
           d='M559.74 905.055C251.002 905.055 -0.000305176 702.455 -0.000305176 452.528C-0.000305176 202.6 251.002 0 559.74 0'
-          data-figma-gradient-fill='{"type":"GRADIENT_DIAMOND","stops":[{"color":{"r":0.48333331942558289,"g":0.0,"b":1.0,"a":1.0},"position":0.0},{"color":{"r":1.0,"g":1.0,"b":1.0,"a":0.0},"position":0.93000000715255737}],"stopsVar":[{"color":{"r":0.48333331942558289,"g":0.0,"b":1.0,"a":1.0},"position":0.0},{"color":{"r":1.0,"g":1.0,"b":1.0,"a":0.0},"position":0.93000000715255737}],"transform":{"m00":-971.33819580078125,"m01":1.0572557505037286e-13,"m02":1046.2271728515625,"m10":-1.1339741672793460e-13,"m11":-905.62286376953125,"m12":900.97039794921875},"opacity":1.0,"blendMode":"NORMAL","visible":true}'
+          data-figma-gradient-fill='{"type":"GRADIENT_DIAMOND","stops":[{"color":{"r":0.48333331942558289,"g":0.0,"b":1.0,"a":1.0},"position":0.0},{"color":{"r":1.0,"g":1.0,"b":1.0,"a":0.0},"position":0.93000000715255737}],"stopsVar":[{"color":{"r":0.48333331942558289,"g":0.0,"b":1.0,"a":1.0},"position":0.0},{"color":{"r":1.0,"g":1.0,"b":1.0,"a":0.0},"position":0.93000000715255737}],"transform":{"m00":-971.33819580078125,"m01":1.0572557505037286e-13,"m02":1046.2271728515625,"m10":-1.1339741672793460e-13,"m11":-905.62286376953125,"m12":900.97039794921875},"opacity":0.80000001192092896,"blendMode":"NORMAL","visible":true}'
         />
         <defs>
           <clipPath id='paint0_diamond_1418_1526_clip_path'>
@@ -82,9 +81,9 @@ const Test = ({ width = 200, height = 200 }) => {
       </svg>
       <svg
         xmlns='http://www.w3.org/2000/svg'
-        width={width}
-        height={height}
-        viewBox='0 0 560 906'
+        width='561'
+        height='906'
+        viewBox='0 0 561 906'
         fill='none'
       >
         <g
@@ -98,7 +97,7 @@ const Test = ({ width = 200, height = 200 }) => {
               width='1157.29'
               height='1012.33'
               fill='url(#paint0_diamond_1418_1527)'
-              opacity='1'
+              opacity='0.8'
               shape-rendering='crispEdges'
             />
             <rect
@@ -108,7 +107,7 @@ const Test = ({ width = 200, height = 200 }) => {
               height='1012.33'
               transform='scale(1 -1)'
               fill='url(#paint0_diamond_1418_1527)'
-              opacity='1'
+              opacity='0.8'
               shape-rendering='crispEdges'
             />
             <rect
@@ -118,7 +117,7 @@ const Test = ({ width = 200, height = 200 }) => {
               height='1012.33'
               transform='scale(-1 1)'
               fill='url(#paint0_diamond_1418_1527)'
-              opacity='1'
+              opacity='0.8'
               shape-rendering='crispEdges'
             />
             <rect
@@ -128,14 +127,14 @@ const Test = ({ width = 200, height = 200 }) => {
               height='1012.33'
               transform='scale(-1)'
               fill='url(#paint0_diamond_1418_1527)'
-              opacity='1'
+              opacity='0.8'
               shape-rendering='crispEdges'
             />
           </g>
         </g>
         <path
           d='M0.740234 905.055C309.478 905.055 560.481 702.455 560.481 452.528C560.481 202.6 309.478 0 0.740234 0'
-          data-figma-gradient-fill='{"type":"GRADIENT_DIAMOND","stops":[{"color":{"r":0.0,"g":0.066666670143604279,"b":1.0,"a":1.0},"position":0.0},{"color":{"r":1.0,"g":1.0,"b":1.0,"a":0.0},"position":0.93000000715255737}],"stopsVar":[{"color":{"r":0.0,"g":0.066666670143604279,"b":1.0,"a":1.0},"position":0.0},{"color":{"r":1.0,"g":1.0,"b":1.0,"a":0.0},"position":0.93000000715255737}],"transform":{"m00":971.33819580078125,"m01":1.0572557505037286e-13,"m02":-485.74676513671875,"m10":1.1339741672793460e-13,"m11":-905.62286376953125,"m12":900.97039794921875},"opacity":1.0,"blendMode":"NORMAL","visible":true}'
+          data-figma-gradient-fill='{"type":"GRADIENT_DIAMOND","stops":[{"color":{"r":0.0,"g":0.066666670143604279,"b":1.0,"a":1.0},"position":0.0},{"color":{"r":1.0,"g":1.0,"b":1.0,"a":0.0},"position":0.93000000715255737}],"stopsVar":[{"color":{"r":0.0,"g":0.066666670143604279,"b":1.0,"a":1.0},"position":0.0},{"color":{"r":1.0,"g":1.0,"b":1.0,"a":0.0},"position":0.93000000715255737}],"transform":{"m00":971.33819580078125,"m01":1.0572557505037286e-13,"m02":-485.74676513671875,"m10":1.1339741672793460e-13,"m11":-905.62286376953125,"m12":900.97039794921875},"opacity":0.80000001192092896,"blendMode":"NORMAL","visible":true}'
         />
         <defs>
           <clipPath id='paint0_diamond_1418_1527_clip_path'>
@@ -158,4 +157,4 @@ const Test = ({ width = 200, height = 200 }) => {
   );
 };
 
-export default Test;
+export default DiamondGradient3;
